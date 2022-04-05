dd666b0c06b35aea50a5d497e17aac8e1585cb11
Brice <bricel67@gmail.com>
2022-04-05 13:54:07 +0200

    amelioration geolocalisation

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
+let villeChoisi;
+
+function erreur() {
+  villeChoisi = 'paris';
+  recupererTemp(villeChoisi);
+}
+
+if ('geolocation' in navigator) {
+  navigator.geolocation.watchPosition((position) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lon=${position.coords.longitude}&lat=${position.coords.latitude}&appid=ce22a50a7aa8b692c7352a57c6b0135d&units=metric`;
+
+    console.log(url);
+
+    let requete = new XMLHttpRequest();
+    requete.open('GET', url);
+    requete.responseType = 'json';
+    requete.send();
+
+    requete.onload = function () {
+      if (requete.readyState === XMLHttpRequest.DONE) {
+        if (requete.status === 200) {
+          let res = requete.response;
+
+          document.querySelector('#ville').textContent = res.name;
+          document.querySelector('#temperature_label').textContent =
+            res.main.temp;
+        }
+      }
+    };
+  }, erreur);
+} else {
+  villeChoisi = 'Paris';
+  recupererTemp(villeChoisi);
+}
+
 let changerVille = document.querySelector('#changer');
 
 changerVille.addEventListener('click', demanderVille);
 
-function recupererTemp(ville = 'sundhoffen') {
+function recupererTemp(ville) {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=ce22a50a7aa8b692c7352a57c6b0135d&units=metric`;
 
   let requete = new XMLHttpRequest();
@@ -21,11 +56,4 @@ function recupererTemp(ville = 'sundhoffen') {
       }
     }
   };
-}
-
-recupererTemp();
-
-function demanderVille() {
-  villeChoisi = prompt('Choisissez la ville que vous souhaitez ?');
-  recupererTemp(villeChoisi);
 }
